f5e559cd93eb4696c3d439fc7757da61d2165173
Александр Альканов <alkanov.alexander@mail.ru>
2022-03-04 21:55:03 +0300

    ht 1

---
+++ src/hw1/Task2.java
@@ -0,0 +1,19 @@
+package hw1;
+
+public class Task2 {
+	public static void main(String[] args) {
+		// для числа 45
+		int a = 45; //для числа 45
+		int b = a / 10;
+		int c = a % 10;
+		int d = b + c; // сумма цифр числа 45
+		System.out.println("Вывод суммы цифр числа 45: " + d);
+		
+		//для числа 11
+		int e = 11;
+		int f = e / 10;
+		int g = e - 10;
+		int h = f + g;
+		System.out.println("Вывод суммы цифр числа 11: " + h);
+	}
+}
---
+++ src/hw1/Task3.java
@@ -0,0 +1,11 @@
+package hw1;
+
+public class Task3 {
+	public static void main(String[] args) {
+		//S = 2(ab + bc + ac)	
+		int a = 3, b = 2, c = 4;
+		int area = 2*(a*b + b*c + a*c);
+		System.out.println(area);
+	}
+
+}
