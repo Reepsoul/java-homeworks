fcc13cd4d3f5d5c3005dea34e21d233ffe0667c5
Alexander <alkanov.alexander@mail.ru>
2022-06-07 16:54:53 +0300

    2

--- src/kursach2/Game.java
+++ src/kursach2/Game.java
@@ -3,18 +3,30 @@ package kursach2;
 public class Game {
 
 	private Text text;
-	
-	
+		
+		
 	public Game() {
 		
 	}
-
+		
 	public Text getText() {
 		return text;
 	}
-
+		
 	public void setText(Text text) {
 		this.text = text;
 	}
-	
+		
+	public void start() {
+		getText();
+	}
+		
 }
+
+
+
+//нужно создать класс игра, где будет ссылка на текущий параграф
+		// в методе старт у текущего параграфа вызвать гет текст
+		// если поьзователь выбирает 1, то текущий параграф ферст - у него вызывается мтод гет текст
+		// если 2- секонд и у него вызывается гет текст
+		// а если пользоваетль введет 3 - то выход в меню
